555ad6bf749d2b56abf536660ee3e870f78d105e
victor.magarlamov <victor.magarlamov@gmail.com>
2020-10-11 14:56:32 +0300

    Updates

--- 2.f3fb0aac813332f2335a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{12:function(n){n.exports=JSON.parse('{"greeting":{"hi":"Hi!"}}')}}]);
--- 3.f3fb0aac813332f2335a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{13:function(n){n.exports=JSON.parse('{"greeting":{"hi":"Привет!"}}')}}]);
